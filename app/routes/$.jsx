0bd9de75c18f1deb4b301ddd8b650936789323de
GhrollorhG <39232926+Iconsana@users.noreply.github.com>
2025-07-02 22:52:46 +0200

    Create $.jsx

---
+++ app/routes/$.jsx
@@ -0,0 +1,15 @@
+// This catch-all route helps with CORS issues
+export const loader = async () => {
+  return new Response(null, {
+    status: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+      "Access-Control-Allow-Headers": "Content-Type, Authorization",
+    },
+  });
+};
+
+export default function CatchAll() {
+  return null;
+}
